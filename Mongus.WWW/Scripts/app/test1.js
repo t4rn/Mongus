344ef129fa7ad8c38bc5d24274b86b7a8296d2a9
t4rn <kwitkowski.it@gmail.com>
2017-11-18 16:02:23 +0100

    test1

---
+++ Mongus.WWW/Scripts/app/test1.js
@@ -0,0 +1,57 @@
+﻿var app = angular.module("myApp", ['ngMaterial', 'ngMessages']);
+
+app.controller("controll", function ($scope, myService) {
+
+
+    $scope.SaveUser = function () {
+
+        var User =
+        {
+            FName: $scope.fName,
+            LName: $scope.lName,
+            Email: $scope.uEmail,
+            Password: $scope.uPwd,
+            UserName: $scope.uName
+        };
+
+        var response = myService.AddUser(User);
+        response.then(function (data) {
+            if (data.data == "1") {
+
+                clearFields();
+                alert("User Created !");
+                window.location.href = "/Register/Login";
+            } else if (data.data == "-1") {
+
+                alert("user already present !");
+            } else {
+
+                clearFields();
+                alert("Invalid data entered !");
+            }
+        });
+    }
+
+    function clearFields() {
+        $scope.fName = "";
+        $scope.lName = "";
+        $scope.uEmail = "";
+        $scope.uPwd = "";
+        $scope.uName = "";
+        $scope.userForm.$setPristine();
+    }
+});
+
+app.service("myService", function ($http) {
+
+    this.AddUser = function (User) {
+        var response = $http
+        ({
+            method: "post",
+            url: "/Register/AddUser",
+            data: JSON.stringify(User),
+            dataType: "json"
+        });
+        return response;
+    };
+});
