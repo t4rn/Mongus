16e4d41b2774914786264b3d0f237ae7e2334b7f
t4rn <kwitkowski.it@gmail.com>
2017-11-27 15:05:59 +0100

    Login + flex removal

--- Mongus.WWW/app/components/login/loginController.js
+++ Mongus.WWW/app/components/login/loginController.js
@@ -8,6 +8,20 @@
     function LoginController() {
 
         var vm = this;
+        var email, passwd = "";
+        var remember = false;
+
+        vm.Signin = Signin;
+        vm.GoogleSignIn = GoogleSignIn;
+
+        function Signin() {
+            var msg = "Email: " + vm.email + " Pass: " + vm.passwd + " rememberme: " + vm.remember;
+            alert(msg);
+        };
+
+        function GoogleSignIn() {
+            alert("Google SignIn clicked!");
+        };
     }
 
 })();
