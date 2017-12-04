1bbd4064e456f8295cd1c4e9026dc659cd8adbda
Gietson <gietson1@gmail.com>
2017-12-04 20:50:53 +0100

    title

--- Mongus.WWW/app/mongusApp.js
+++ Mongus.WWW/app/mongusApp.js
@@ -3,74 +3,81 @@ angular.module("mongusApp", ["ngMaterial", "ngRoute", "chart.js", "ui.router"])
 .config(function ($routeProvider, $locationProvider, $mdThemingProvider, $provide, $mdIconProvider, $stateProvider) {
 
     $stateProvider
-        .state("Home", {
-            url: "/",
-            templateUrl: "/app/components/main/mainContent.html"
+      .state("Home", {
+          title: 'Home',
+          url: "/",
+          templateUrl: "/app/components/main/mainContent.html"
         })
-        .state("Values", {
-            url: "/values",
-            controller: "valuesController",
-            controllerAs: "vm",
-            templateUrl: "/app/components/values/values.html"
+      .state("Values", {
+          title: "Values",
+          url: "/values",
+          controller: "valuesController",
+          controllerAs: "vm",
+          templateUrl: "/app/components/values/values.html"
         })
-        .state("Register", {
-            url: "/register",
-            controller: "registerController",
-            controllerAs: "vm",
-            templateUrl: "/app/components/register/register.html"
+      .state("Register", {
+          title: "Sign Up",
+          url: "/register",
+          controller: "registerController",
+          controllerAs: "vm",
+          templateUrl: "/app/components/register/register.html"
         })
-        .state("Users", {
-            url: "/users",
-            controller: "usersController",
-            //controllerAs: "vm",
-            templateUrl: "/app/components/users/users.html"
+      .state("Users", {
+          title: "Users",
+          url: "/users",
+          controller: "usersController",
+          //controllerAs: "vm",
+          templateUrl: "/app/components/users/users.html"
         })
-        .state("Clients", {
-            url: "/clients",
-            controller: "clientsController",
-            //controllerAs: "vm",
-            templateUrl: "/app/components/clients/clients.html"
+      .state("Clients", {
+          title: "Clients",
+          url: "/clients",
+          controller: "clientsController",
+          //controllerAs: "vm",
+          templateUrl: "/app/components/clients/clients.html"
         })
-        .state("Charts", {
-            url: "/charts",
-            controller: "chartsController",
-            //controllerAs: "vm",
-            templateUrl: "/app/components/charts/charts.html"
+      .state("Charts", {
+          title: "Chartus",
+          url: "/charts",
+          controller: "chartsController",
+          //controllerAs: "vm",
+          templateUrl: "/app/components/charts/charts.html"
         })
-        .state("ChartsGoogle", {
-            url: "/chartsGoogle",
-            controller: "chartsGoogleController",
-            controllerAs: "vm",
-            tempateUrl: "/app/components/chartsGoogle/chartsGoogle.html"
+      .state("ChartsGoogle", {
+          title: "Google Charts",
+          url: "/chartsGoogle",
+          controller: "chartsGoogleController",
+          controllerAs: "vm",
+          tempateUrl: "/app/components/chartsGoogle/chartsGoogle.html"
         })
-        .state("HighCharts", {
-            url: "/highCharts",
-            controller: "highChartsController",
-            //controllerAs: "vm",
-            templateUrl: "/app/components/highCharts/highCharts.html"
+      .state("HighCharts", {
+          title: "Hight Charts",
+          url: "/highCharts",
+          controller: "highChartsController",
+          //controllerAs: "vm",
+          templateUrl: "/app/components/highCharts/highCharts.html"
         });
 
 
     $mdThemingProvider.theme('default')
-        .primaryPalette('blue')
-        .accentPalette('brown');
+      .primaryPalette('blue')
+      .accentPalette('brown');
 
     $mdThemingProvider.theme('altTheme')
-        .primaryPalette('orange')
-        .warnPalette('green')
-        .accentPalette('pink');
+      .primaryPalette('orange')
+      .warnPalette('green')
+      .accentPalette('pink');
 
     $provide.value('themeProvider', $mdThemingProvider);
 
     $mdIconProvider.defaultIconSet("/app/assets/svg/avatars.svg", 128);
 
-});
+  });
 
 app.run(['$rootScope', '$state', '$stateParams', '$log', function ($rootScope, $state, $stateParams, $log) {
 
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-
     //$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
 
     //    $log.debug('successfully changed states');
@@ -97,6 +104,10 @@ app.run(['$rootScope', '$state', '$stateParams', '$log', function ($rootScope, $
         $log.debug('toParams', toParams);
         $log.debug('fromState', fromState);
         $log.debug('fromParams', fromParams);
+    });
+
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+    $rootScope.title = toState.title;
     });
 
 }]);
