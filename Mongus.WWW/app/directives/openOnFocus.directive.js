35fe8f3875a2e5bb404bd242e81ba662bf5f3580
t4rn <kwitkowski.it@gmail.com>
2017-12-29 13:00:26 +0100

    Open dropdown on focus directive

---
+++ Mongus.WWW/app/directives/openOnFocus.directive.js
@@ -0,0 +1,52 @@
+﻿
+(function () {
+
+    "use strict";
+
+    angular.module("app").directive("openOnFocus", openOnFocus);
+
+    function openOnFocus() {
+        return {
+            scope: true,
+            restrict: 'A',
+            link: function (scope, elem, attr) {
+
+                scope.showOptions = true;
+                let lock = false;
+
+                if ((attr['mdOnClose'])) {
+                    attr['mdOnClose'] = "showOptions=false;" + (attr['mdOnClose']);
+                } else {
+                    (attr['mdOnClose']) = "showOptions=false;"
+                }
+
+                elem.on('focus', function (event) {
+                    lock = false;
+
+                    if (scope.showOptions) {
+                        //console.log("[focus] launching click event!");
+                        elem.triggerHandler('click');
+                    }
+                    else {
+                        //console.log("[focus] doing nothing...");
+                    }
+                });
+
+                elem.on('click', function (event) {
+                    if (lock) {
+                        event.stopImmediatePropagation();
+                    }
+                    else {
+                        lock = true;
+                    }
+                });
+
+
+                elem.on('blur', function () {
+                    scope.showOptions = true;
+                });
+            }
+        };
+    };
+
+})();
